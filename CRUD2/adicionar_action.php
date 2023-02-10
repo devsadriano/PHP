8f841553de0aa1af5f05b97e12ce0363a107227c
Adriano Amador Rocha <adriano.rocha@PMCG.IMTI>
2023-02-10 10:48:53 -0400

    FIM d aula Criando um DAO na prática (3/5)

--- CRUD2/adicionar_action.php
+++ CRUD2/adicionar_action.php
@@ -1,37 +1,29 @@
 <?php
 require 'config.php';
+require 'dao/UsuarioDAOMySQL.php';
+
+$usuarioDao = new UsuarioDAOMySQL($pdo);
 
 $name = filter_input(INPUT_POST, 'name');
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
 if ($name && $email) {
-    $sql = $pdo->prepare("SELECT * FROM schemaphp.usuarios WHERE email = $email");
-    $sql->bindValue(':email', $email);
-    $sql->execute();
 
-    if ($sql->rowCount() === 0) {
-        $sql = $pdo->prepare("INSERT INTO schemaphp.usuarios (name, email) VALUES(:name, :email)"); // e um template
-        $sql->bindValue(':name', $name); 
-        $sql->bindValue(':email', $email);
-        $sql->execute();        
+    if ($usuarioDao->findByEmail($email) === false) {
+        $novoUsuario = new Usuario();
+        $novoUsuario ->setNome($name);
+        $novoUsuario ->setEmail($email);
+
+        $usuarioDao->add($novoUsuario);
+
         header("Location: index.php");
         exit;
-    }else {
+    } else {
         header('Location: adicionar.php');
         exit;
     }
+
+    }else {
+        header('Location: adicionar.php');
+        exit;
 }
-
-// procedimento q tem varias etapas de seguranca basicamente vmaos mntando aos poucos a nossa query e depois executa pdp statement = $pdo->prepare
-//  ->prepare()  aq mando o esboço dessa query  
-//  bindValue() 1 parametro e quem quero modificar  2 parametro  vai trasformar $name no valor q ta if($name) e jogar no VALUES(:name)
-// bindValue() associa o valor q mando
-// bindParam() associa o parametro ou seja estou associando a variavel  o q quer dizer  se depois de eu delcarar eu posso torcar 
-// sempre q acidionar um item eu volto pro index 
-
-
-//No processo de create, qual das alternativas a seguir é primordial para que os dados vão para o banco de dados, assim como foi preenchido pelo cliente ?
-//Query de inserção dos dados que foi pego no cliente através do formulário.
-
-//[PHP] - Para que não haja duplicidade na inserção de usuário com o mesmo e-mail, é necessário utilizar o método X para verificar a existência do e-mail dentro da tabela. Marque a opção que representa o X da declaração acima.
-// rowCount()
--- CRUD2/dao/UsuarioDAOMySQL.php
+++ CRUD2/dao/UsuarioDAOMySQL.php
@@ -9,7 +9,13 @@ class UsuarioDAOMySQL implements UsuarioDAO{
     }
 
     public function add(Usuario $u){
+        $sql = $this->pdo->prepare("INSERT INTO schemaphp.usuarios (name, email) VALUES(:name, :email)");
+        $sql->bindValue(':name', $u->getNome());
+        $sql->bindValue(':email', $u->getEmail());
+        $sql->execute();
 
+        $u->setId($this->pdo->lastInsertId());
+        return $u;
     }
 
     public function findAll(){
@@ -28,6 +34,24 @@ class UsuarioDAOMySQL implements UsuarioDAO{
             }
         }
         return $array;
+    }
+
+    public function findByEmail($email){
+        $sql = $this->pdo->prepare("SELECT * FROM schemaphp.usuarios WHERE email = '$email'");
+        $sql->bindValue(':email',$email);
+        $sql->execute();
+        if ($sql->rowCount() > 0) {
+            $data = $sql->fetch();
+
+            $u = new Usuario();
+            $u->setId($data['id']);
+            $u->setNome($data['name']);
+            $u->setEmail($data['email']);
+
+            return $u;
+        } else {
+            return false;
+        }
     }
 
     public function findById($id){
